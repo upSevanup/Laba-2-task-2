99d64a7b54b9adbe3c0b6caece03413a0f3aab6d
Sevan <Sevan@INBOOK_X2_GEN11>
2023-09-20 19:18:08 +0700

    Добавьте файлы проекта.

---
+++ лаба 2 задание 2/лаба 2 задание 2.cpp
@@ -0,0 +1,19 @@
+﻿//Задание 2
+
+#include <iostream>
+#include <clocale>
+
+using namespace std;
+
+int main() {
+	setlocale(LC_ALL, "ru");
+
+	int n;
+
+	cout << "Введите трехзначное число: ";
+	cin >> n;
+
+	n = (n / 100) * 100;
+
+	cout << "Число с занулёнными еденицами и десятками: " << n;
+}
